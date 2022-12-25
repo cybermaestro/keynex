03020da63986293035dc49cd885795ba8b4346e9
cybermaestro <superman1499@mail.ru>
2022-12-25 15:50:28 +0300

    добавил секцию service

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import 'bootstrap';
 import common from './common/js/common';
 import header from './modules/header/header';
 import './assets/icons/icons';
+import { createEmitAndSemanticDiagnosticsBuilderProgram } from "typescript";
 
 common();
 header();
