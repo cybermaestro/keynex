d3f3de2266d55cd1bbe55454f6cb35d5d37fa814
cybermaestro <superman1499@mail.ru>
2023-01-06 20:58:56 +0300

    закончил blog и добавил страницу career

--- src/modules/header/header.js
+++ src/modules/header/header.js
@@ -1,3 +1,5 @@
+//Карта на страницу contact.html
+
 export default function() {
   const header = document.querySelector('.header');
 }
@@ -16,4 +18,35 @@ function init() {
 	});
   
   map.geoObjects.add(placemark);
+}
+
+//открывющееся окна на кейсы со страницы career
+
+const open = document.querySelectorAll('.open-case');
+const window = document.querySelectorAll('.window');
+const cross = document.querySelectorAll('.cross');
+const windowCon = document.querySelectorAll('.window__content');
+
+
+
+function handleClickFunctionCase(i) {
+  window[i].classList.toggle('hide');
+}
+
+for (let index = 0; index < open.length; index++) {
+  open[index].addEventListener('click', function () {handleClickFunctionCase(index)}); //открытие по нажатию на блок
+  cross[index].addEventListener('click', function () {handleClickFunctionCase(index)}); //закрытие по нажати на крест
+  addEventListener('keydown', function (event) {
+    if (event.keyCode == 27) {
+      window[index].classList.add('hide');
+    }
+  });    //закрытие окна при нажатии на Esc
+
+  window[index].addEventListener('click', function (e) {
+    let con = e.target.closest('.window__content');
+    if (con == null) {;
+      window[index].classList.add('hide');
+    }
+  });   //закрытие окна при нажатии на фон 
+
 }
